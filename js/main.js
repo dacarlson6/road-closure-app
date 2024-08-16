f4291928c1c3e867582aff63b4f0f642ccafbdc2
dacarlson6 <dacarlson6@wisc.edu>
2024-08-16 07:12:13 -0500

    update max zoom

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function createMap(){
 
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
-        maxZoom: 18
+        maxZoom: 20
     }).addTo(map);
 
     getData(map);
