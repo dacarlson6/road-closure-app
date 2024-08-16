bec761fa4b72edd2f696b42a6cbd7ea9440d9270
dacarlson6 <dacarlson6@wisc.edu>
2024-08-16 07:09:48 -0500

    update topo map url

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ function createMap(){
         zoom: 13
     });
 
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
-        maxZoom: 20
+        maxZoom: 18
     }).addTo(map);
 
     getData(map);
