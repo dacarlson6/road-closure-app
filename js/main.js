aa3f1e6ab97e8bb309a6b3be4411a2ad67d656e6
dacarlson6 <dacarlson6@wisc.edu>
2024-08-21 12:47:05 +0000

    debug modal not closing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     createMap();
+    setupModal();
 });
 
 function createMap() {
@@ -13,7 +14,7 @@ function createMap() {
         maxZoom: 20
     }).addTo(map);
 
-    // Data fetching and GeoJSON integration
+    //data fetching and GeoJSON integration
     fetch('http://18.214.48.54:8000/roadclosures')
         .then(response => response.json())
         .then(data => {
@@ -28,43 +29,24 @@ function createMap() {
         })
         .catch(error => console.error('Error loading the road closures data:', error));
 
-    setupControls(map);
-}
-
-function setupControls(map) {
-    // Geocoder and Home button setup
-    var geocoder = L.Control.geocoder({
-        defaultMarkGeocode: false,
-        collapsed: false
-    }).on('markgeocode', function(e) {
-        var bbox = e.geocode.bbox;
-        L.polygon([bbox.getSouthEast(), bbox.getNorthEast(), bbox.getNorthWest(), bbox.getSouthWest()]).addTo(map);
-        map.fitBounds(bbox);
-    }).addTo(map);
-
-    geocoder.getContainer().querySelector('.leaflet-control-geocoder-icon').click();
-
-    var homeControl = new L.Control.HomeButton({
-        position: 'topleft'
-    }).addTo(map);
-    homeControl.addTo(map);
-
-    setupModal();
 }
 
 function setupModal() {
     var modal = document.getElementById("welcomeModal");
     var closeButton = document.getElementById("closeModal");
 
+    //display the modal by default
     modal.style.display = "block";
 
-    closeButton.onclick = function() {
+    //close modal when 'Explore' button is clicked
+    closeButton.addEventListener('click', function() {
         modal.style.display = "none";
-    };
+    });
 
-    window.onclick = function(event) {
-        if (event.target == modal) {
+    //close modal if clicked outside of modal content
+    window.addEventListener('click', function(event) {
+        if (event.target === modal) {
             modal.style.display = "none";
         }
-    };
+    });
 }
